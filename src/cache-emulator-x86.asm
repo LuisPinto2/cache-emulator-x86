f29b675ce6bdf2b607242ba44cd34371c4ed1ddb
Luís Pinto <fc60831@alunos.fc.ul.pt>
2026-07-20 02:05:47 +0100

    Added english comments

--- src/cache-emulator-x86.asm
+++ src/cache-emulator-x86.asm
@@ -1,7 +1,7 @@
 ; fc60831
 
 SECTION .data
-    msg_erro db "Argumento(s) inválido(s)." ; Mensagem de erro
+    msg_erro db "Invalid argument(s)."      ; Error message
     msg_len equ $ - msg_erro                ; Length msg_erro
     
 SECTION .text
@@ -16,91 +16,91 @@ SECTION .text
 _start:
     mov rbp, rsp
 
-    ; Ler e validar o nr de args (argc)
+    ; Read and validate args number (argc)
     mov r15, [rbp]                      ; r15 = argc
     cmp r15, 1
-    jle _erro                           ; Se argc <= 1 então exibe erro
+    jle _erro                           ; If argc <= 1 then show error
 
-    dec r15                             ; Como tirámos argv[0] restam (argc - 1) argumentos para ler
-    mov r14, 0                          ; Contador r14 começa em 0
+    dec r15                             ; Removed argv[0] so theres (argc - 1) arguments left to read
+    mov r14, 0                          ; Counter r14 begins at 0
     
 _leitura:
-    ; Verificar se todos os argumentos foram processados
+    ; Verify if all args have been processed
     cmp r14, r15
     je _exit
     
-    ; Ler argv[r14]
-    mov rax, [rbp + 16 + r14*8]         ; Ponteiro para o próximo argumento/string
-    inc r14                             ; Incrementar contador de argumentos processados
+    ; Read argv[r14]
+    mov rax, [rbp + 16 + r14*8]         ; Pointer to the next argument
+    inc r14                             ; Increment processed arguments counter
     
     mov rbx, [rax]
     xor rax, rax
-    mov ax, bx                          ; ax = endereço da cache
+    mov ax, bx                          ; ax = cache address
             
-    ; Obter offset em r13b
+    ; Get offset in r13b
     mov r13b, 11b
     and r13b, al
     
-    ; Obter index em r12b
+    ; Get index in r12b
     mov r12b, 111100b
-    and r12b, al                        ; Aplica a máscara
-    shr r12b, 2                         ; Restam apenas os 4 bits do index
+    and r12b, al                        ; Apply the mask
+    shr r12b, 2                         ; Theres only 4 bits left from the index
     
-    ; Obter tag em bx
+    ; Get tag in bx
     mov bx, 1111111111000000b
-    and bx, ax                          ; Aplica a máscara
-    shr bx, 6                           ; Restam apenas os 10 bits da tag
+    and bx, ax                          ; Apply the mask
+    shr bx, 6                           ; Theres only 10 bits left from the tag
     
-    ; Verificar bit de validade
+    ; Verify validation bit
     movzx edi, r12b
 
-    call get_validation_bit             ; Obter bit de validade em al
+    call get_validation_bit             ; Get validation bit in al
     call display_table
     
-    ; Se o bit de validade for 0, logo ocorre miss
+    ; If validation bit is 0, then miss
     test al, al                         ; Verificar se o bit de validade é 0 ou 1
     jz _validation_bit                  ; Se al = 0 ocorreu miss
     
-    ; Caso bit de validade =! 0 ainda há a possibilidade de ocorrer miss (bit validade = 1 & tags diferentes)
+    ; If validation bit =! 0 theres still chance of being a miss (validity bit = 1 & different tags)
     movzx edi, r12b
-    call get_tag                        ; Obter tag em rax
+    call get_tag                        ; Get tag in rax
 
-    cmp ax, bx                          ; Comparar o valor da tag que já estava na cache com o valor da tag do argumento dado
+    cmp ax, bx                          ; Compare tag value that was in cache to argument tag
     jne _miss
     
-    ;Se nenhum destes acontecer então ocorre hit
+    ; If any of these happen then hit
     jmp _hit
 
     
 _validation_bit:
     movzx edi, r12b
-    call set_validation_bit             ; Atualizar o valor do bit de validade
+    call set_validation_bit             ; Update validation bit
     call display_table
 
     
 _miss:
     movzx rdi, r12b                     ; rdi = index
     movzx rsi, bx                       ; rsi = tag
-    call set_tag                        ; Inserir o valor da tag
+    call set_tag                        ; Insert tag value
         
     movzx rdi, r12b                     ; rdi = index
     movzx rsi, r13b                     ; rsi = offset
-    call get_data                       ; Obter data da cache
+    call get_data                       ; Get cache data
     call display_table
 
-    jmp _iteracao                       ; Salta o bloco _hit para não duplicar leitura
+    jmp _iteracao                       ; Jumps _hit block not to read twice
             
 _hit:
     movzx rdi, r12b
     movzx rsi, r13b
-    call get_data                       ; Obter data da cache
+    call get_data                       ; Get cache data
     call display_table
     
 _iteracao:
-    jmp _leitura                        ; Há de ser um jump para leitura para ler e processar os outros argumentos (aumentando r9)
+    jmp _leitura
     
 _erro:
-    ; Imprimir mensagem de erro
+    ; Print error message
     mov rdi, 2
     lea rsi, [msg_erro]
     mov rdx, msg_len
